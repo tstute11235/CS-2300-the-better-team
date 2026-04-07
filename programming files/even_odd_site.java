313705a99a88144eac31d8ab4062a96d92456fc5
Grady Nautel <gnautel@uvm.edu>
2026-04-07 10:31:10 -0400

    Added comments explaining code

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -5,8 +5,10 @@ import java.io.*;
 public class even_odd_site {
     public static void main(String[] args) throws IOException {
         BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
+        //Write the Title
         file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
         file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
+        //For each number, if it is even write it on the left column and if it is odd write on the right column
         for (int i = 1; i <= 50; i++) {
             if (i % 2 == 0) {
                 file.write("<tr><td>" + i + "</td><td></td></tr>\n");
@@ -19,6 +21,7 @@ public class even_odd_site {
         file.close();
         BufferedReader reader = new BufferedReader(new FileReader("numbers.html"));
         String line;
+        //Print out each line in the table
         while ((line = reader.readLine()) != null) {
             System.out.println(line);
         }
