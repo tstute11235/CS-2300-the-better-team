75f81e36c539a4efd0fb426003955cd306194ee2
Christian Paul <christianspaul72@gmail.com>
2026-04-07 10:30:35 -0400

    Enhance remove_duplicates function to track removed items
    Added functionality to track removed duplicate elements.

--- programming files/remove_duplicates.py
+++ programming files/remove_duplicates.py
@@ -1,10 +1,14 @@
+# Function to remove duplicates from an array
 def remove_duplicates(arr):
     arr.sort()
     unique_arr = []
     removed_elements = []
+
     for i in range(len(arr)):
+        # If value not already seen, add to unique list
         if i == 0 or arr[i] != arr[i-1]:
             unique_arr.append(arr[i])
+        # Otherwise add to remove list
         else:
             removed_elements.append(arr[i])
     return unique_arr, removed_elements
